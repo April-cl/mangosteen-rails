d6c6c4870a767f8a4cf06523835c58447b32fa70
April-cl <xcl8202@gmail.com>
2023-04-13 14:48:05 +0800

    实现 users 的 create 和 show 方法

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,21 @@
 class UsersController < ApplicationController
   def show
+    user = User.find_by_id params[:id]
+    if user
+      render json: user
+    else
+      head 404
+    end
   end
 
   def create
+    user = User.new name: 'April'
+    if user.save
+      p 'save 成功'
+      render json: user
+    else
+      p 'save 失败'
+      render json: user.errors
+    end
   end
 end
