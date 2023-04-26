80fecb902404bee3921f32c8d24e3e38139f8bfd
April-cl <xcl8202@gmail.com>
2023-04-26 16:59:09 +0800

    测试邮件被成功发送

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,8 @@
 class UserMailer < ApplicationMailer
 
-  def welcome_email(code)
-    @code = code
-    mail(to: 'xcl8202@qq.com', subject: 'Welcome to My Awesome Site')
+  def welcome_email(email)
+    validation_code = ValidationCode.find_by_email(email)
+    @code = validation_code.code
+    mail(to: email, subject: '山竹记账验证码')
   end
 end
